15dcda4743728a75f4b3dd07b113f55c7c9c49a7
Abdallah Ayman <Abdallah5919771@gmail.com>
2026-07-31 09:22:45 +0300

    Add double slider and make mini price and max price filter work in url

--- src/components/PriceFilter/PriceFilter.jsx
+++ src/components/PriceFilter/PriceFilter.jsx
@@ -1,23 +1,35 @@
+import { Slider } from "@/components/ui/slider";
 import { useAppContext } from "../../../context/context";
+import { useState } from "react";
 
 export default function PriceFilter() {
+  const [range, setRange] = useState([800, 2500]);
+
   const { maxPriceValue, updateParams } = useAppContext();
 
-  function changePrice(value) {
-    updateParams({ maxPrice: value });
+  function changePrice(minValue, maxValue) {
+    updateParams({ miniPrice: minValue, maxPrice: maxValue });
+  }
+
+  function getRange(value) {
+    setRange(value);
+    changePrice(range[0], range[1]);
   }
 
   return (
     <div>
-      <h3 className="font-semibold mb-4">Price: 500, {maxPriceValue}</h3>
+      <h3 className="font-semibold mb-4">
+        Price: {range[0]}, {range[1]}
+      </h3>
 
-      <input
-        type="range"
+      <Slider
+        value={range}
+        onValueChange={(value) => getRange(value)}
+        defaultValue={[800, 2000]}
         min={500}
         max={6000}
-        className="w-full"
-        value={maxPriceValue}
-        onChange={(e) => changePrice(e.target.value)}
+        step={5}
+        className="mx-auto w-full max-w-xs"
       />
 
       <div className="flex justify-between text-sm mt-2">
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -7,5 +11,10 @@ export default defineConfig({
   server: {
     host: true, // same as '0.0.0.0' - exposes to your local network
     port: 5173, // optional, just to fix the port
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
 });

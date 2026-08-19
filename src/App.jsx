49e928c1eaf6830e52d69f15c5e745641f4c8028
Abdallah Ayman <Abdallah5919771@gmail.com>
2026-08-19 21:09:55 +0300

    Add transation

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,22 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home";
 import Form from "./Form/form";
 import OtpPage from "./otpPage/otpPage";
+import { motion } from "framer-motion";
 
 export default function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/form" element={<Form />} />
-        <Route path="/form/otp" element={<OtpPage />} />
-      </Routes>
+      <motion.div
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5, ease: "easeInOut" }}
+      >
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/form" element={<Form />} />
+          <Route path="/form/otp" element={<OtpPage />} />
+        </Routes>
+      </motion.div>
     </>
   );
 }
